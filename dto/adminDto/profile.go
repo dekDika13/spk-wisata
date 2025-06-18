23e208a0d5852c1eeef4d60efac0329dfb16ef5b
DikaRama <kadekdikaramasuryadi@gmail.com>
2025-06-18 13:56:14 +0800

    edit_dto_profile

--- dto/adminDto/profile.go
+++ dto/adminDto/profile.go
@@ -3,16 +3,22 @@ package admindto
 import "mime/multipart"
 
 type ProfileDTO struct {
-	UserId   uint   `json:"user_id" validate:"required"`
-	FullName string `json:"full_name" validate:"required"`
-	Photo    string `json:"photo" validate:"required"`
-	Bod      string `json:"bod" validate:"required"`
-	Address  string `json:"address" validate:"required"`
+	UserId   uint   `json:"user_id" `
+	FullName string `json:"full_name" `
+	Photo    string `json:"photo" `
+	Bod      string `json:"bod" `
+	Address  string `json:"address" `
+	Role     uint   `json:"role" `
+	Username string `json:"username" `
+	Email    string `json:"email" `
+	Phone    string `json:"phone" `
 }
 type InsertProfileDTO struct {
 	UserId   uint                  `json:"user_id" validate:"required"`
-	FullName string                `json:"full_name" validate:"required"`
+	FullName string                `json:"full_name" `
 	Photo    *multipart.FileHeader `form:"photo"`
-	Bod      string                `json:"bod" validate:"required"`
-	Address  string                `json:"address" validate:"required"`
+	Bod      string                `json:"bod" `
+	Address  string                `json:"address" `
+	Username string                `json:"username"`
+	Password string                `json:"password" validate:"required"`
 }
